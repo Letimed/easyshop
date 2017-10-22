db9d45c35bae7fb7704369d1e11d4c6684f788f8
Vieirakuh <quentin.vieira@epitech.eu>
2017-10-22 23:48:28 +0200

    UNIREST WORKING

--- Project/easyshop/src/pages/CallApi/CallApi.ts
+++ Project/easyshop/src/pages/CallApi/CallApi.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {PeopleServiceProvider} from '../../providers/people-service/people-service';
+declare var require: any
 
 
 @Component({
@@ -13,14 +14,17 @@ export class CallApi {
 
  public people: any;
   constructor(public navCtrl: NavController,public peopleService: PeopleServiceProvider) {
-    this.loadPeople();
-     // let unirest = require('unirest');
-     /*let request = unirest.get("https://datagram-products-v1.p.mashape.com/chains/")
-    .header("X-Mashape-Key", "ThOXlrzNNOmshsnKuXV73dC9bFEyp1qa5EJjsnZ96p0ss8gGO3")
+    //this.loadPeople();
+    // These code snippets use an open-source library. http://unirest.io/nodejs
+
+    var unirest = require('unirest');
+    // These code snippets use an open-source library. http://unirest.io/nodejs
+    unirest.get("https://datagram-products-v1.p.mashape.com/chains/")
+    .header("X-Mashape-Key", "q75JmrJtUImshhVN83hCaxDvQELbp1C5oAXjsnhNMVAXlTf1uh")
     .header("Accept", "application/json")
     .end(function (result) {
-      console.log(result.status, result.headers, result.body);
-    });*/
+    console.log(result.status, result.headers, result.body);
+    });
     }
 
   loadPeople(){
