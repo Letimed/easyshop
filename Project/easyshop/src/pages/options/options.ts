6c194d476273b29a9fa61e1de7d3b495b44652b6
Vieirakuh <quentin.vieira@epitech.eu>
2017-10-30 16:32:29 +0100

    Delete Product and verification

---
+++ Project/easyshop/src/pages/options/options.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-option',
+  templateUrl: 'options.html',
+})
+
+export class OptionPage {
+
+  constructor(public navCtrl: NavController) {}
+
+}
