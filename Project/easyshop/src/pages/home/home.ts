4409999c3532e98db1bf450000192e7c10438e06
camus-_f <florian.camus-martin@epitech.eu>
2017-11-02 15:58:47 +0100

    clean repos

--- Project/easyshop/src/pages/home/home.ts
+++ Project/easyshop/src/pages/home/home.ts
@@ -6,7 +6,6 @@ import { OptionPage } from '../options/options';
 import { ProduitPage } from '../produits/produits';
 import { RecettePage } from '../Recette/recette';
 import { mesRecettes } from '../mesRecettes/mesRecettes';
-import { Push, PushObject, PushOptions } from '@ionic-native/push';
 
 @Component({
   selector: 'page-home',
@@ -17,7 +16,7 @@ export class HomePage {
   userProfile: any = null;
 
 
-  constructor(public navCtrl: NavController, private push: Push) {}
+  constructor(public navCtrl: NavController) {}
 
   geoLocButton() {
   	this.navCtrl.push(NavigationPage);
@@ -51,38 +50,5 @@ export class HomePage {
   	console.log("click facebookButton");
   }
 
-  pushNotif()
-  {
-  this.push.hasPermission().then((res: any) => {
-
-    if (res.isEnabled) {
-      console.log('We have permission to send push notifications');
-    } else {
-      console.log('We do not have permission to send push notifications');
-    }
-
-  });
-
-  const options: PushOptions = {
-   android: {},
-   ios: {
-       alert: 'true',
-       badge: true,
-       sound: 'false'
-   },
-   windows: {},
-   browser: {
-       pushServiceURL: 'http://push.api.phonegap.com/v1/push'
-   }
-   };
-
-   const pushObject: PushObject = this.push.init(options);
-
-     pushObject.on('notification').subscribe((notification: any) => console.log('Received a notification', notification));
-
-    pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
-
-    pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
-  }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # easyshop
 easyshop
+
+Projet :
+
+Le projet easyshop est une partie d'un projet de plus grand envergure qui donnera suite a une application plus conséquente.
+De ce fait, nous avons mis une page login avec facebook qui sera par la suite retirée de l'application (car aucun sens dans le projet finale) dans le but de montrer que nous avons compris la notion de connection + Oauth.
+
+Le but de l'application est de générer une liste de course.
+Cette liste de course sera générée automatiquement par l'application grâce aux recettes réalisées pas l'utilisateur au préalable.
+L'application sera dans la mesure de trouver toutes les grandes surfaces se trouvant autour de l'utilisateur.
+
+Pluggin utilisés :
+storage
+geolocalisation
+firebase
+splashscreen
+Oauth
+
+API utilisées:
+googlemap
+mastercourse
+facebook
+
+groupe numero :
+membre groupe : Quentin Vieira / Florian Camus-Martin
+Nous avons tout deux assistés au cours module hybrid.
