fe7a2ced89fd399b5516ebb1c488214ddd276164
Quentin V <quentin.vieira@epitech.eu>
2018-02-16 15:53:47 +0100

    Begin Design Ionic Serve Working

--- Project/easyshop/src/pages/home/home.ts
+++ Project/easyshop/src/pages/home/home.ts
@@ -23,7 +23,7 @@ export class HomePage {
     //
     // ONESIGNAL INIT
     //
-    this.oneSignal.startInit('f1c036d3-cd14-411b-846b-d5400c9edcc1', '378512581486');
+    /*this.oneSignal.startInit('f1c036d3-cd14-411b-846b-d5400c9edcc1', '378512581486');
     this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
     this.oneSignal.handleNotificationReceived().subscribe(() => {
     });
@@ -31,7 +31,7 @@ export class HomePage {
     this.oneSignal.handleNotificationOpened().subscribe(() => {
     });
 
-    this.oneSignal.endInit();
+    this.oneSignal.endInit();*/
     //
     //ONE SIGNAL END
     //
